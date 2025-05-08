47ba08e3995a1ef23fabeb5179a27fccd666bb0e
Giovanna Lopes <giovanna.lsantos28@gmail.com>
2025-05-08 10:54:09 -0300

    Tela Cadastro Funcional
    Criei a tela cadastro e implementei com o banco de dados.

--- pages/cadastro.php
+++ pages/cadastro.php
@@ -28,28 +28,48 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
-<!-- formulario de teste -->
 
-
-<!-- <!DOCTYPE html>
-<html lang="pt-BR">
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Cadastro de Usuário</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/cadastro.css">
+    <title>Cadastro | CortAí</title>
 </head>
 <body>
-    <h2>Cadastro de Usuário</h2>
-    <form method="POST" action="">
-        <label>Nome:</label><br>
-        <input type="text" name="nome" required><br><br>
-
-        <label>Email:</label><br>
-        <input type="email" name="email" required><br><br>
-
-        <label>Senha:</label><br>
-        <input type="password" name="senha" required><br><br>
-
-        <button type="submit">Cadastrar</button>
-    </form>
+    <div class="cadastro-box">
+        <div class="cadastro-header">
+            <header>Cadastro</header>
+        </div>
+        <form action="cadastro.php" method="POST"> <!-- Formulário com método POST -->
+            <div class="input-box">
+                <input type="text" class="input-field" placeholder="Nome" name="nome" autocomplete="off" required>
+            </div>
+            <div class="input-box">
+                <input type="email" class="input-field" placeholder="Email" name="email" autocomplete="off" required>
+            </div>
+            <div class="input-box">
+                <input type="password" class="input-field" placeholder="Senha" name="senha" autocomplete="off" required>
+            </div>
+            <div class="forgot">
+                <section>
+                    <input type="radio" id="cliente" name="perfil" value="cliente" class="animated-radio" checked>
+                    <label for="cliente">Sou cliente</label>
+                </section>
+                <section>
+                    <input type="radio" id="barbeiro" name="perfil" value="barbeiro" class="animated-radio">
+                    <label for="barbeiro">Sou barbeiro</label>
+                </section>              
+            </div>
+            <div class="input-submit">
+                <button type="submit" class="submit-btn">Cadastrar</button>
+            </div>
+        </form>
+        <div class="sign-up-link">
+            <p>Já tem uma conta? <a href="login.html">Entrar</a></p>
+        </div>
+    </div>
 </body>
-</html> -->
+</html>
