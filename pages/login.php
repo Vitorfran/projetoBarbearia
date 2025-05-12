51ce5042389ac370ab958457ed8065171e09bcc9
Vitorfran <vitorpoizon@gmail.com>
2025-05-12 17:17:38 -0300

    Atualizando Tela de Login

--- pages/login.php
+++ pages/login.php
@@ -1,5 +1,3 @@
-
-=======
 <?php
 session_start();
 require_once __DIR__ . '/../config/database.php';
@@ -41,45 +39,49 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
->>>>>>> Stashed changes
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/login.css">
     <title>Login | CortAí</title>
+    <link rel="stylesheet" href="../assets/css/login.css">
 </head>
 <body>
     <div class="cadastro-box">
         <div class="cadastro-header">
             <header>Login</header>
         </div>
-        <div class="input-box">
-            <input type="text" class="input-field" placeholder="Nome" autocomplete="off" required>
-        </div>
-        <div class="input-box">
-            <input type="text" class="input-field" placeholder="Email" autocomplete="off" required>
-        </div>
-        <div class="input-box">
-            <input type="password" class="input-field" placeholder="Senha" autocomplete="off" required>
-        </div>
-        <div class="forgot">
-            <section>
-                <input type="checkbox" id="check">
-                <label for="check">Lembrar usuário</label>
-            </section>
-            <section>
-                <a href="#">Esqueceu sua senha?</a>
-            </section>
-        </div>
-        <div class="input-submit">
-            <button class="submit-btn" id="submit"></button>
-            <label for="submit">Entrar</label>
-        </div>
+        
+        <?php if (isset($_SESSION['erro'])): ?>
+            <div class="alert alert-danger">
+                <?= $_SESSION['erro']; unset($_SESSION['erro']); ?>
+            </div>
+        <?php endif; ?>
+        
+        <form method="POST" action="login.php">
+            <div class="input-box">
+                <input type="email" name="email" class="input-field" placeholder="Email" required>
+            </div>
+            <div class="input-box">
+                <input type="password" name="senha" class="input-field" placeholder="Senha" required>
+            </div>
+            <div class="forgot">
+                <section>
+                    <input type="checkbox" id="check" name="lembrar">
+                    <label for="check">Lembrar usuário</label>
+                </section>
+                <section>
+                    <a href="recuperar_senha.php">Esqueceu sua senha?</a>
+                </section>
+            </div>
+            <div class="input-submit">
+                <button type="submit" class="submit-btn">Entrar</button>
+            </div>
+        </form>
+        
         <div class="sign-up-link">
-            <p>Não tem uma conta? <a href="../pages/cadastro.php">Criar conta</a></p>
+            <p>Não tem uma conta? <a href="cadastro.php">Criar conta</a></p>
         </div>
     </div>
 </body>
